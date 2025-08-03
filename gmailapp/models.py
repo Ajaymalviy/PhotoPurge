bc417658d90b6d4c57faa89db5e1714ad5b6bb65
t <ajeymalviya143@gmail.com>
2025-08-03 23:26:53 +0530

    remove baseabstract

--- gmailapp/models.py
+++ gmailapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, baseAstractUser
+from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now, localtime
 from django.contrib.auth.models import User
 
